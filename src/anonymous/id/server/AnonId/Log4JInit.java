fdaf8d5d91bf96f5f7641d4324115d7640116c3e
longpork <Anon183830@hushmail.com>
2011-07-28 00:25:43 -0400

    Added libraries that will obviously be needed. jstl for easy scriptless jsp log4j for easy logging Also added Requirements.html to DevDocs to document where these come from so they can be easily upgraded as needed

---
+++ src/anonymous/id/server/AnonId/Log4JInit.java
@@ -0,0 +1,46 @@
+package anonymous.id.server.AnonId;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.PropertyConfigurator;
+
+/**
+ * Servlet implementation class Log4JInit
+ */
+public class Log4JInit extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	  public void init() {
+	    String prefix =  getServletContext().getRealPath("/");
+	    String file = getInitParameter("log4j-init-file");
+	    // if the log4j-init-file is not set, then no point in trying
+	    if(file != null) {
+	      PropertyConfigurator.configure(prefix+file);
+	    }
+	  }
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Log4JInit() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// This should never be called
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// This should never be called
+	}
+
+}
