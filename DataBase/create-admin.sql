772f7dcf7f12e29407b98cecbc467d5cdb8e6e76
longpork <Anon183830@hushmail.com>
2011-08-02 22:26:11 -0400

    A name like admin is a dead giveaway. May as well use UID 0.

--- DataBase/create-admin.sql
+++ DataBase/create-admin.sql
@@ -1,6 +1,5 @@
 use AnonID;
-select (FLOOR(1 + (RAND() * 2147483646))) into @newid;
-insert into users (id, name, status) values (@newid, 'admin', 'ACTIVE');
-insert into shadow (uid, password, type) values (@newid, PASSWORD(CONCAT('admin','admin')), 'ADMIN');
-insert into shadow (uid, password, type) values (@newid, PASSWORD(CONCAT('admin','norm')), 'NORMAL');
-insert into shadow (uid, password, type) values (@newid, PASSWORD(CONCAT('admin','duress')), 'DURESS');
+insert into users (id, name, status) values (0, 'admin', 'ACTIVE');
+insert into shadow (uid, password, type) values (0, PASSWORD(CONCAT('admin','admin')), 'ADMIN');
+insert into shadow (uid, password, type) values (0, PASSWORD(CONCAT('admin','norm')), 'NORMAL');
+insert into shadow (uid, password, type) values (0, PASSWORD(CONCAT('admin','duress')), 'DURESS');
