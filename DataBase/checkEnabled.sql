b39521ec77aadfc4e5a2fcf471619ced0d76852d
longpork <longporkdev@yahoo.com>
2011-09-17 02:08:48 -0400

    procedure to check if an enabled auth cookie is valid

--- DataBase/checkEnabled.sql
+++ DataBase/checkEnabled.sql
@@ -11,7 +11,10 @@ IN lc BIGINT(20),
 IN ec BIGINT(20)
 )
 BEGIN
-	
+	SELECT (authCookieIsValid(lc) && authCookieIsValid(ec)) STATUS
+	FROM (SELECT * FROM authCookies WHERE id = lc AND type = 'LOGIN') lct
+	JOIN (SELECT * FROM authCookies WHERE id = ec AND type = 'ADMIN') ect
+	ON lct.userid = ect.userid;
 END $$
 
 DELIMITER ;
