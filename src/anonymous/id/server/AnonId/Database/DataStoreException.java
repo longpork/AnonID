bd108131686d2beb6e9b288ff360c7a46bc5781a
longpork <longporkdev@yahoo.com>
2011-09-19 13:28:50 -0400

    Added serialVersionUID to remove Warning Added getMessage() to remove warning and make sure our messages can be retrieved.

--- src/anonymous/id/server/AnonId/Database/DataStoreException.java
+++ src/anonymous/id/server/AnonId/Database/DataStoreException.java
@@ -1,9 +1,15 @@
 package anonymous.id.server.AnonId.Database;
 
 public class DataStoreException extends Exception {
+	private static final long serialVersionUID = 1L;
 	private String Message;
 	public DataStoreException(String string) {
 		Message = string;
+	}
+	
+	@Override
+	public String getMessage() {
+		return Message;
 	}
 
 }
